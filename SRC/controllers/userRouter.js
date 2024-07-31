1aea3187dd02a1ffeafb17bffc25c3abd2473ba0
gysagsohn <gysagsohn@hotmail.com>
2024-07-31 14:32:15 +1000

    feature: router added additional paths

--- SRC/controllers/userRouter.js
+++ SRC/controllers/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { UserModel } = require("../models/UserModel")
 const router = express.Router()
 
+//ground work for controller and can add models to this
 router.get("/", async (request, response, next) =>{
 
     let result = await UserModel.find({}).exec();
@@ -11,6 +12,33 @@ router.get("/", async (request, response, next) =>{
         result: result
     });
 
-});
+}); 
+
+router.get("./:id", (request, response, next) => {
+    response.json({
+        message:"User Route is working"
+    });
+})
+
+router.post("./:id", (request, response, next) => {
+    response.json({
+        message:"User Route is working"
+    });
+})
+
+
+router.put("./:id", (request, response, next) => {
+    response.json({
+        message:"User Route is working"
+    });
+})
+
+
+router.delete("./:id", (request, response, next) => {
+    response.json({
+        message:"User Route is working"
+    });
+})
+
 
 module.exports = router;
